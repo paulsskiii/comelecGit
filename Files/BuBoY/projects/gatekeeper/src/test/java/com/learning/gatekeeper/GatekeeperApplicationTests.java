75f9d6e6b26dc351df99dbcaba9424ee31aeb4f5
felimonenrile-creator <felimon.enrile@gmail.com>
2026-02-06 15:42:57 +0800

    the other half day files 02062026

---
+++ Files/BuBoY/projects/gatekeeper/src/test/java/com/learning/gatekeeper/GatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.gatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Files/BuBoY/projects/polyglot/src/main/java/com/learning/polyglot/controller/DashboardController.java
@@ -0,0 +1,19 @@
+
+package com.learning.polyglot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/")
+    public String showDashboard(Model model) {
+        // We pass a dynamic value "Alex" to the model.
+        // We want the result to be "Welcome, Alex!" or "¡Bienvenido, Alex!"
+        model.addAttribute("username", "Alex");
+        
+        return "dashboard";
+    }
+}
