97c006936d59df9ee17befc5c387b25c3352fa87
ctbuison <ctbuison@gmail.com>
2026-02-09 13:54:21 +0800

    smart wallet PM

---
+++ Files/Charles/Day 6/smartwallet_pm/src/test/java/com/learning/smartwallet/SmartwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.smartwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
